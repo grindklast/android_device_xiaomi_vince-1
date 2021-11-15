05fed0a3eee002fa485a19053904a3759a0a9c84
rayrzy <vincentrayananda@gmail.com>
2021-11-15 14:00:19 +0700

    vince: NAD-ify
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>

---
+++ nad_vince.mk
@@ -0,0 +1,50 @@
+#
+# Copyright (C) NusantaraProject
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from vince device
+$(call inherit-product, device/xiaomi/vince/device.mk)
+
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vince
+PRODUCT_NAME := nad_vince
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 5 Plus
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
+NAD_BUILD_TYPE := OFFICIAL
+USE_PIXEL_CHARGING := true
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# GApps
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# NusantaraPapers
+PRODUCT_PACKAGES += \
+    NusantaraPapers
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+rm -rf hardware/qcom-caf/msm8996/media
+rm -rf hardware/qcom-caf/msm8996/display
+git clone https://github.com/rayrzy/platform_hardware_qcom_display hardware/qcom-caf/msm8996/display
+git clone https://github.com/rayrzy/platform_hardware_qcom_media hardware/qcom-caf/msm8996/media
